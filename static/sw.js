9183f3a438dafce61daa78016d085503c31d941a
LensConnect254 <joram.mburun@gmail.com>
2026-07-01 22:25:16 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,2 +1,3 @@
-self.addEventListener('install', e=>e.waitUntil(caches.open('v1').then(c=>c.addAll(['/']))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {});
